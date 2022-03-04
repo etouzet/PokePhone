779ae4ce82149e825de761f2ef197ac49728cce1
touzet <evantouzet31@gmail.com>
2022-03-04 09:44:22 +0100

    Ajout de la recherche d'image dans la gallerie

--- PokePhone/PokePhone/Pages/AjoutPokemon.xaml.cs
+++ PokePhone/PokePhone/Pages/AjoutPokemon.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Plugin.Media;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -17,13 +19,43 @@ namespace PokePhone.Pages
             InitializeComponent();
         }
 
+        async void AjouterImage(object sender, System.EventArgs e)
+        {
+            //! added using Plugin.Media;
+            await CrossMedia.Current.Initialize();
+
+            if (!CrossMedia.Current.IsPickPhotoSupported)
+            {
+                await DisplayAlert("Not supported", "Your device does not currently support this functionality", "Ok");
+                return;
+
+            }
+
+            var mediaOptions = new PickMediaOptions()
+            {
+                PhotoSize = PhotoSize.Medium
+            };
+            // if you want to take a picture use TakePhotoAsync instead of PickPhotoAsync
+            var selectedImageFile = await CrossMedia.Current.PickPhotoAsync(mediaOptions);
+
+            this.saisieImage.Source = ImageSource.FromStream(() => selectedImageFile.GetStream());
+
+        }
+
         public void BtnAjouterPokemon(object sender, EventArgs args)
         {
-            if (this.saisieHp.Text != "")
+
+            if (this.saisieHp.Text != "" || this.saisieAttack.Text != "" || this.saisieDefense.Text != "" || this.saisieNom.Text != "")
             {
                 this.saisieHp.PlaceholderColor = Color.Red;
+                this.saisieAttack.PlaceholderColor = Color.Red;
+                this.saisieDefense.PlaceholderColor = Color.Red;
+                this.saisieNom.PlaceholderColor = Color.Red;
+                this.saisieType.TitleColor = Color.Red;
+
             }
             
         }
+
     }
 }
